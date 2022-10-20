9de05a58c69e62c16ca7c36c7e3a831a01f08536
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2022-10-20 23:18:14 +0300

    Select correct libraries and include files based on OS and CPU arch

--- libasi-sys/build.rs
+++ libasi-sys/build.rs
@@ -4,18 +4,35 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let path = std::fs::canonicalize("./vendors/zwo-sdk/linux/lib/x64");
+    let platform = match env::consts::FAMILY {
+        "unix" => "linux",
+        "windows" => "windows",
+        _ => panic!("Unsupported operating system"),
+    };
+
+    let arch = match env::consts::ARCH {
+        "x86" => "x86",
+        "x86_64" => "x64",
+        "arm" => "armv7",
+        "aarch64" => "armv8",
+        _ => panic!("Unsupported CPU architecture"),
+    };
+
+    let library_path =
+        std::fs::canonicalize(format!("./vendors/zwo-sdk/{}/lib/{}", platform, arch)).unwrap();
+    let header_path = format!("./vendors/zwo-sdk/{}/include/ASICamera2.h", platform);
+
     // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search={}", path.unwrap().display());
+    println!("cargo:rustc-link-search={}", library_path.display());
 
     // Tell cargo to tell rustc to link the library.
     println!("cargo:rustc-link-lib=ASICamera2");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=./vendors/zwo-sdk/linux/include/ASICamera2.h");
+    println!("cargo:rerun-if-changed={}", header_path);
 
     let bindings = bindgen::Builder::default()
-        .header("./vendors/zwo-sdk/linux/include/ASICamera2.h")
+        .header(header_path)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
