9b4091df3be25d3b7cca578bf2612fcd7e196ba6
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-04-08 20:29:17 +0300

    Add placeholder functions for opening and closing camera

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,32 @@ fn main() {
     // Start camera thread
     let r = Arc::clone(&running);
     let camera_thread = thread::spawn(move || {
+        let cam = open_camera();
+
         while r.load(Ordering::SeqCst) {
             println!("Camera thread is working...");
             thread::sleep(Duration::from_secs(1));
         }
-        println!("Camera thread exiting cleanly.");
+
+        close_camera(cam);
+        println!("Camera thread exited cleanly.");
     });
 
     camera_thread.join().expect("Camera thread panicked!");
+}
+
+struct Camera {}
+impl Camera {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+fn close_camera(_cam: Camera) {
+    // TODO: Implement camera closing logic
+}
+
+fn open_camera() -> Camera {
+    // TODO: Implement camera opening logic
+    Camera::new()
 }
