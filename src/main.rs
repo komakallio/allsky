ee067e0c1291d40efdedf62df1be781730ca827e
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-04-08 20:19:09 +0300

    Start placeholder camera thread
    It doesn't do anything yet, except keep checking whether Ctrl+C has been
    pressed. Once it has, it stops the thread.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Flag used to signal all threads to stop working
+    let running = Arc::new(AtomicBool::new(true));
+
+    // Clone the flag for the signal handler
+    let r = Arc::clone(&running);
+    ctrlc::set_handler(move || {
+        println!("Received Ctrl+C!");
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl+C handler");
+
+    // Start camera thread
+    let r = Arc::clone(&running);
+    let camera_thread = thread::spawn(move || {
+        while r.load(Ordering::SeqCst) {
+            println!("Camera thread is working...");
+            thread::sleep(Duration::from_secs(1));
+        }
+        println!("Camera thread exiting cleanly.");
+    });
+
+    camera_thread.join().expect("Camera thread panicked!");
 }
