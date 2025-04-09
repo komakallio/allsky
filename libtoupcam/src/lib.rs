c543e2fd3a4c2271192c03548c845c504a21cfc7
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-04-09 22:50:34 +0300

    Add alias for libtoupcam_sys in libtoupcam
    This prevents name collisions between libtoupcam and libtoupcam_sys in
    the main.rs file.

--- libtoupcam/src/lib.rs
+++ libtoupcam/src/lib.rs
@@ -1,25 +1,27 @@
-use libtoupcam_sys::*;
+use libtoupcam_sys as toup;
+
 #[cfg(not(target_family = "windows"))]
 use std::os::raw::c_chart;
 #[cfg(target_family = "windows")]
 use std::os::raw::c_ushort;
 
 #[derive(Debug)]
-pub struct ToupcamDetails {
+pub struct ToupcamDevice {
     pub id: String,
     pub display_name: String,
 }
 
-pub fn enumerate_cameras() -> Vec<ToupcamDetails> {
-    let mut cam_array = [unsafe { std::mem::zeroed::<ToupcamDeviceV2>() }; TOUPCAM_MAX as usize];
-    unsafe { Toupcam_EnumV2(cam_array.as_mut_ptr()) };
+pub fn enumerate_cameras() -> Vec<ToupcamDevice> {
+    let mut cam_array =
+        [unsafe { std::mem::zeroed::<toup::ToupcamDeviceV2>() }; toup::TOUPCAM_MAX as usize];
+    unsafe { toup::Toupcam_EnumV2(cam_array.as_mut_ptr()) };
 
     let mut cameras = Vec::new();
-    for i in 0..TOUPCAM_MAX as usize {
+    for i in 0..toup::TOUPCAM_MAX as usize {
         if cam_array[i].displayname[0] == 0 {
             break;
         }
-        cameras.push(ToupcamDetails {
+        cameras.push(ToupcamDevice {
             id: characters_to_string(&cam_array[i].id),
             display_name: characters_to_string(&cam_array[i].displayname),
         });
@@ -27,16 +29,16 @@ pub fn enumerate_cameras() -> Vec<ToupcamDetails> {
     cameras
 }
 
-pub fn open() -> HToupCam {
-    unsafe { Toupcam_Open(std::ptr::null_mut()) }
+pub fn open() -> toup::HToupCam {
+    unsafe { toup::Toupcam_Open(std::ptr::null_mut()) }
 }
 
-pub fn open_by_index(index: usize) -> HToupCam {
-    unsafe { Toupcam_OpenByIndex(index as u32) }
+pub fn open_by_index(index: usize) -> toup::HToupCam {
+    unsafe { toup::Toupcam_OpenByIndex(index as u32) }
 }
 
-pub fn close(cam: HToupCam) {
-    unsafe { Toupcam_Close(cam) }
+pub fn close(cam: toup::HToupCam) {
+    unsafe { toup::Toupcam_Close(cam) }
 }
 
 #[cfg(target_family = "windows")]
